98c96035d2f6a03972b72c2ecc0345626f4f6307
maryamkhaled25 <72885891+maryamkhaled25@users.noreply.github.com>
2023-09-18 13:47:16 +0300

    last

--- main.go
+++ main.go
@@ -1,3 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+func moveDisks(n int, source, aux, target *Stack) {
+	if n > 0 {
+		// Move n-1 disks from source to aux using target as the auxiliary stack
+		moveDisks(n-1, source, target, aux)
+
+		// Move the nth disk from source to target
+		target.push(source.pop())
+		fmt.Printf("Move disk %v from stick %v to stick %v \n", n, source.id, target.id)
+		// Move the n-1 disks from aux to target using source as the auxiliary stack
+		moveDisks(n-1, aux, source, target)
+	}
+}
+
+type Stack struct {
+	items []int
+	id    string
+}
+
+func (s *Stack) push(item int) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) pop() int {
+	if len(s.items) == 0 {
+		return -1 // Stack is empty
+	}
+
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
+
+func main() {
+	var nDisks int
+	_, err := fmt.Scanln(&nDisks)
+	if err != nil {
+		return
+	}
+	source := &Stack{id: "1"}
+	for i := nDisks; i > 0; i-- {
+		source.push(i)
+	}
+	aux := &Stack{id: "2"}
+	target := &Stack{id: "3"}
+
+	moveDisks(len(source.items), source, aux, target)
+
+	fmt.Println("Source stack:", source.items)
+	fmt.Println("Auxiliary stack:", aux.items)
+	fmt.Println("Target stack:", target.items)
+}
+
 // package main
 //
 // import "fmt"
@@ -61,60 +119,3 @@
 //		fmt.Printf("The number of disks is %d , Solve the problem with steps", numDisks)
 //		towerOfHanoi(numDisks)
 //	}
-package main
-
-import (
-	"fmt"
-)
-
-func moveDisks(n int, source, aux, target *Stack) {
-	if n > 0 {
-		// Move n-1 disks from source to aux using target as the auxiliary stack
-		moveDisks(n-1, source, target, aux)
-
-		// Move the nth disk from source to target
-		target.push(source.pop())
-		fmt.Printf("Move disk %v from stick %v to stick %v \n", n, source.id, target.id)
-		// Move the n-1 disks from aux to target using source as the auxiliary stack
-		moveDisks(n-1, aux, source, target)
-	}
-}
-
-type Stack struct {
-	items []int
-	id    string
-}
-
-func (s *Stack) push(item int) {
-	s.items = append(s.items, item)
-}
-
-func (s *Stack) pop() int {
-	if len(s.items) == 0 {
-		return -1 // Stack is empty
-	}
-
-	item := s.items[len(s.items)-1]
-	s.items = s.items[:len(s.items)-1]
-	return item
-}
-
-func main() {
-	var nDisks int
-	_, err := fmt.Scanln(&nDisks)
-	if err != nil {
-		return
-	}
-	source := &Stack{id: "1"}
-	for i := nDisks; i > 0; i-- {
-		source.push(i)
-	}
-	aux := &Stack{id: "2"}
-	target := &Stack{id: "3"}
-
-	moveDisks(len(source.items), source, aux, target)
-
-	fmt.Println("Source stack:", source.items)
-	fmt.Println("Auxiliary stack:", aux.items)
-	fmt.Println("Target stack:", target.items)
-}
